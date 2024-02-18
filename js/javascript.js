9a7341e284655b63b678e509e4a772586508076c
Giovanni Useche <giovanni.useche@hotmail.com>
2024-02-18 00:29:27 -0500

    I update the JavaScript with all the word combinations and add the events to all the 5 buttons

--- js/javascript.js
+++ js/javascript.js
@@ -69,37 +69,33 @@ const textcard2 = document.querySelector('#text-card-2');
 const imgcard2 = document.querySelector('#img-card-2');
 
 function random_card_2() {
-  var random_number = Math.floor(Math.random() * 7) + 1;
+  var random_number = Math.floor(Math.random() * 6) + 1;
 
   switch (random_number) {
     case 1:
-      src = '../img/the-turkey.jpg';
-      text = 'The turkey';
+      src = '../img/sat-on.jpg';
+      text = 'sat on';
       break;
     case 2:
-      src = '../img/mom.jpg';
-      text = 'Mon';
+      src = '../img/ate.jpg';
+      text = 'ate';
       break;
     case 3:
-      src = '../img/dad.jpg';
-      text = 'Dad';
+      src = '../img/danced-with.jpg';
+      text = 'danced with';
       break;
     case 4:
-      src = '../img/the-dog.jpg';
-      text = 'The dog';
+      src = '../img/saw.jpg';
+      text = 'saw';
       break;
     case 5:
-      src = '../img/the-teacher.jpg';
-      text = 'The teacher';
+      src = "../img/doesnt-like.jpg"
+      text = "doesn't like";
       break;
     case 6:
-      src = '../img/the-elephant.jpg';
-      text = 'The elephant';
+      src = '../img/kissed.jpg';
+      text = 'kissed';
       break;
-    case 7:
-      src = '../img/the-cat.jpg';
-      text = 'The cat';
-      break;  
     default:
       console.log("NAN");
   }
@@ -124,37 +120,33 @@ const textcard3 = document.querySelector('#text-card-3');
 const imgcard3 = document.querySelector('#img-card-3');
 
 function random_card_3() {
-  var random_number = Math.floor(Math.random() * 7) + 1;
+  var random_number = Math.floor(Math.random() * 6) + 1;
 
   switch (random_number) {
     case 1:
-      src = '../img/the-turkey.jpg';
-      text = 'The turkey';
+      src = '../img/a-funny.jpg';
+      text = 'a funny';
       break;
     case 2:
-      src = '../img/mom.jpg';
-      text = 'Mon';
+      src = '../img/a-scary.jpg';
+      text = 'a scary';
       break;
     case 3:
-      src = '../img/dad.jpg';
-      text = 'Dad';
+      src = '../img/a-goofy.jpg';
+      text = 'a goofy';
       break;
     case 4:
-      src = '../img/the-dog.jpg';
-      text = 'The dog';
+      src = '../img/a-slimy.jpg';
+      text = 'a slimy';
       break;
     case 5:
-      src = '../img/the-teacher.jpg';
-      text = 'The teacher';
+      src = '../img/a-barking.jpg';
+      text = 'a barking';
       break;
     case 6:
-      src = '../img/the-elephant.jpg';
-      text = 'The elephant';
+      src = '../img/a-fat.jpg';
+      text = 'a fat';
       break;
-    case 7:
-      src = '../img/the-cat.jpg';
-      text = 'The cat';
-      break;  
     default:
       console.log("NAN");
   }
@@ -183,32 +175,32 @@ function random_card_4() {
 
   switch (random_number) {
     case 1:
-      src = '../img/the-turkey.jpg';
-      text = 'The turkey';
+      src = '../img/goat.jpg';
+      text = 'goat';
       break;
     case 2:
-      src = '../img/mom.jpg';
-      text = 'Mon';
+      src = '../img/monkey.jpg';
+      text = 'monkey';
       break;
     case 3:
-      src = '../img/dad.jpg';
-      text = 'Dad';
+      src = '../img/fish.jpg';
+      text = 'fish';
       break;
     case 4:
-      src = '../img/the-dog.jpg';
-      text = 'The dog';
+      src = '../img/cow.jpg';
+      text = 'cow';
       break;
     case 5:
-      src = '../img/the-teacher.jpg';
-      text = 'The teacher';
+      src = '../img/frog.jpg';
+      text = 'frog';
       break;
     case 6:
-      src = '../img/the-elephant.jpg';
-      text = 'The elephant';
+      src = '../img/bug.jpg';
+      text = 'bug';
       break;
     case 7:
-      src = '../img/the-cat.jpg';
-      text = 'The cat';
+      src = '../img/worm.jpg';
+      text = 'worm';
       break;  
     default:
       console.log("NAN");
@@ -234,36 +226,32 @@ const textcard5 = document.querySelector('#text-card-5');
 const imgcard5 = document.querySelector('#img-card-5');
 
 function random_card_5() {
-  var random_number = Math.floor(Math.random() * 7) + 1;
+  var random_number = Math.floor(Math.random() * 6) + 1;
 
   switch (random_number) {
     case 1:
-      src = '../img/the-turkey.jpg';
-      text = 'The turkey';
+      src = '../img/on-the-moon.jpg';
+      text = 'on the moon';
       break;
     case 2:
-      src = '../img/mom.jpg';
-      text = 'Mon';
+      src = '../img/on-the-chair.jpg';
+      text = 'on the chair';
       break;
     case 3:
-      src = '../img/dad.jpg';
-      text = 'Dad';
+      src = '../img/in-my-spaghetti.jpg';
+      text = 'in my spaghetti';
       break;
     case 4:
-      src = '../img/the-dog.jpg';
-      text = 'The dog';
+      src = '../img/in-my-soup.jpg';
+      text = 'in my soup';
       break;
     case 5:
-      src = '../img/the-teacher.jpg';
-      text = 'The teacher';
+      src = '../img/on-the-grass.jpg';
+      text = 'on the grass';
       break;
     case 6:
-      src = '../img/the-elephant.jpg';
-      text = 'The elephant';
-      break;
-    case 7:
-      src = '../img/the-cat.jpg';
-      text = 'The cat';
+      src = '../img/in-my-shoes.jpg';
+      text = 'in my shoes';
       break;  
     default:
       console.log("NAN");
